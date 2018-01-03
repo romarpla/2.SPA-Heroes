21919063d071311565984ba05173d842bdf474a2
RODRIGO\r <jrodrigo.martinez@hotmail.com>
2018-01-03 17:38:34 -0600

    redirect_heroes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'heroes', component: HeroesComponent },
+    //Para pasar un parámetro se le pone /:parametro para usarlo al momento de darle click, este es el que cachas 
+    //en el ts del componente que se redirigirá, en este caso el heroe.component.ts
+    { path: 'heroe/:id', component: HeroeComponent },
     { path: '**', pathMatch:'full',redirectTo: 'home' }
 
     //{ path: 'path/:routeParam', component: MyComponent },
---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+//Para poder usar el servicio se tiene que importar y así poder diferenciar el heroe que queremos
+import { HeroesService } from '../../services/heroes.service'
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent {
+  //Se importa como en cualquier otro componente
+  heroe:any = {};
+
+  constructor( private activatedRoute:ActivatedRoute,
+    private _heroesService:HeroesService ) {
+    //Esto es para recibir los parámetros que se mandan por la URL
+    this.activatedRoute.params.subscribe(params => {
+      this.heroe = this._heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
+    })
+   }
+
+  ngOnInit() {
+  }
+
+}
