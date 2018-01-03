f29b40f40d650206a98f16971348a58ca950735b
RODRIGO\r <jrodrigo.martinez@hotmail.com>
2018-01-03 13:26:15 -0600

    router_links

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: '**', pathMatch:'full',redirectTo: 'home' }
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(ROUTES);
+
