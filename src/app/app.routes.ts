329eff2e2495eb9e012bf665c142629ab8a1f24a
RODRIGO\r <jrodrigo.martinez@hotmail.com>
2018-01-05 11:02:15 -0600

    Complete_search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
@@ -12,6 +13,7 @@ const ROUTES: Routes = [
     //Para pasar un parámetro se le pone /:parametro para usarlo al momento de darle click, este es el que cachas 
     //en el ts del componente que se redirigirá, en este caso el heroe.component.ts
     { path: 'heroe/:id', component: HeroeComponent },
+    { path: 'buscar/:termino', component: BusquedaComponent },
     { path: '**', pathMatch:'full',redirectTo: 'home' }
 
     //{ path: 'path/:routeParam', component: MyComponent },
